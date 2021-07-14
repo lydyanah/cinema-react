134d8dbb5bc1d24b9cdc32fd0eefcf13bbb272ea
Lidi <lidiana.tdsc@gmail.com>
2021-07-14 20:20:57 -0300

    Atualização do Busca.css

--- src/Busca.js
+++ src/Busca.js
@@ -3,9 +3,9 @@ import './Busca.css';
 
 function Busca() {
     return (
-        <div class="root">
+        <div className="root">
             <nav>
-                <a href="index.html" class="brand">Cinema</a>
+                <a href="index.html" className="brand">Cinema</a>
                 <a href="busca.html">Busca</a>
                 <a href="perfil.html">Perfil</a>
                 <a href="contato.html">Contato</a>
@@ -17,19 +17,19 @@ function Busca() {
                 </form>
                 <ul>
                     <li>
-                        <img class="poster" src="https://infinitaeph.com.br/wp-content/uploads/2018/10/20128877.jpg" alt="" />
-                        <span class="title">The Matrix</span>
-                        <span class="year">1999</span>
+                        <img className="poster" src="https://infinitaeph.com.br/wp-content/uploads/2018/10/20128877.jpg" alt="" />
+                        <span className="title">The Matrix</span>
+                        <span className="year">1999</span>
                     </li>
                     <li>
-                        <img class="poster" src="https://infinitaeph.com.br/wp-content/uploads/2018/10/20128877.jpg" alt="" />
-                        <span class="title">The Matrix</span>
-                        <span class="year">1999</span>
+                        <img className="poster" src="https://infinitaeph.com.br/wp-content/uploads/2018/10/20128877.jpg" alt="" />
+                        <span className="title">The Matrix</span>
+                        <span className="year">1999</span>
                     </li>
                     <li>
-                        <img class="poster" src="https://infinitaeph.com.br/wp-content/uploads/2018/10/20128877.jpg" alt="" />
-                        <span class="title">The Matrix</span>
-                        <span class="year">1999</span>
+                        <img className="poster" src="https://infinitaeph.com.br/wp-content/uploads/2018/10/20128877.jpg" alt="" />
+                        <span className="title">The Matrix</span>
+                        <span className="year">1999</span>
                     </li>
                 </ul>
 	        </main>    
